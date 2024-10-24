2f0d7121f8662e6187fa4d3cd6d8a951ae6a755d
ameenajaleel1816 <ameenajaleel1816@gmail.com>
2024-10-24 10:42:09 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n) 
+{
     int i, j, temp;
     int swapped;
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++) 
+    {
         swapped = 0;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+        for (j = 0; j < n - i - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
                 
                 temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -21,8 +25,10 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
+void printArray(int arr[], int size) 
+{
+    for (int i = 0; i < size; i++)
+        {
         printf("%d ", arr[i]); 
     }
     printf("\n");
